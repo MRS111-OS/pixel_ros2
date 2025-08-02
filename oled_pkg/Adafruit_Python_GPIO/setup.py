d0c20ca5d5545b45fc80ebabda357bed4667c251
Yash <yash@momentumrobotics.in>
2025-08-02 16:25:39 +0530

    changed setup files for oled dependencies

--- oled_pkg/Adafruit_Python_GPIO/setup.py
+++ oled_pkg/Adafruit_Python_GPIO/setup.py
@@ -1,11 +1,3 @@
-print('Adafruit GPIO Library')
-print('Works best with Python 2.7')
-print('THIS INSTALL SCRIPT MAY REQUIRE ROOT/ADMIN PERMISSIONS')
-print('Especially if you installed python for "all users" on Windows')
-print('\ntry the following in your systems terminal if ensurepip is not sufficient:')
-print('$ python -m ensurepip --upgrade')
-print('$ python -m pip install --upgrade pip setuptools')
-
 import sys
 try:
     import pip
@@ -23,13 +15,25 @@ requires = ['adafruit-pureio']
 if sys.platform != 'win32' and sys.platform != 'darwin':
     requires.append('spidev')
 
-setup(name              = 'Adafruit_GPIO',
-      version           = '1.0.4',
-      author            = 'Tony DiCola',
-      author_email      = 'tdicola@adafruit.com',
-      description       = 'Library to provide a cross-platform GPIO interface on the Raspberry Pi and Beaglebone Black using the RPi.GPIO and Adafruit_BBIO libraries.',
-      license           = 'MIT',
-      url               = 'https://github.com/adafruit/Adafruit_Python_GPIO/',
-      install_requires  = requires,
-      test_suite        = 'tests',
-      packages          = find_packages())
+setup(
+    name='Adafruit_GPIO',
+    version='1.0.4',
+    author='Tony DiCola',
+    author_email='tdicola@adafruit.com',
+    description='Library to provide a cross-platform GPIO interface on the Raspberry Pi and Beaglebone Black using the RPi.GPIO and Adafruit_BBIO libraries.',
+    license='MIT',
+    url='https://github.com/adafruit/Adafruit_Python_GPIO/',
+    install_requires=requires,
+    test_suite='tests',
+    packages=find_packages()
+)
+
+# Only print help messages when executed directly, not during colcon dry-run
+if __name__ == '__main__':
+    print('Adafruit GPIO Library')
+    print('Works best with Python 2.7')
+    print('THIS INSTALL SCRIPT MAY REQUIRE ROOT/ADMIN PERMISSIONS')
+    print('Especially if you installed python for "all users" on Windows')
+    print('\ntry the following in your system\'s terminal if ensurepip is not sufficient:')
+    print('$ python -m ensurepip --upgrade')
+    print('$ python -m pip install --upgrade pip setuptools')
--- oled_pkg/Adafruit_Python_SSD1306/setup.py
+++ oled_pkg/Adafruit_Python_SSD1306/setup.py
@@ -4,9 +4,12 @@ from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-# Import the README and use it as the long-description.
-with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = '\n' + f.read()
+# Try to read README.md, but fall back if it's not available
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = ''
 
 classifiers = ['Development Status :: 4 - Beta',
                'Operating System :: POSIX :: Linux',
@@ -17,15 +20,18 @@ classifiers = ['Development Status :: 4 - Beta',
                'Topic :: Software Development',
                'Topic :: System :: Hardware']
 
-setup(name              = 'Adafruit_SSD1306',
-      version           = '1.6.2',
-      author            = 'Tony DiCola',
-      author_email      = 'tdicola@adafruit.com',
-      description       = 'Python library to use SSD1306-based 128x64 or 128x32 pixel OLED displays with a Raspberry Pi or Beaglebone Black.',
-      long_description  = long_description,
-      license           = 'MIT',
-      classifiers       = classifiers,
-      url               = 'https://github.com/adafruit/Adafruit_Python_SSD1306/',
-      dependency_links  = ['https://github.com/adafruit/Adafruit_Python_GPIO/tarball/master#egg=Adafruit-GPIO-0.6.5'],
-      install_requires  = ['Adafruit-GPIO>=0.6.5'],
-      packages          = find_packages())
+setup(
+    name='Adafruit_SSD1306',
+    version='1.6.2',
+    author='Tony DiCola',
+    author_email='tdicola@adafruit.com',
+    description='Python library to use SSD1306-based 128x64 or 128x32 pixel OLED displays with a Raspberry Pi or Beaglebone Black.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    classifiers=classifiers,
+    url='https://github.com/adafruit/Adafruit_Python_SSD1306/',
+    dependency_links=['https://github.com/adafruit/Adafruit_Python_GPIO/tarball/master#egg=Adafruit-GPIO-0.6.5'],
+    install_requires=['Adafruit-GPIO>=0.6.5'],
+    packages=find_packages()
+)
