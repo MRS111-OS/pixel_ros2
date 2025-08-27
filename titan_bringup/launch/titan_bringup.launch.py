18a561f8a84bd4fffaa4587d343c54ff15f9ca1c
Yash <yash@momentumrobotics.in>
2025-08-27 18:47:29 +0530

    V3 lidar udev, box filter and params update

--- titan_bringup/launch/titan_bringup.launch.py
+++ titan_bringup/launch/titan_bringup.launch.py
@@ -21,7 +21,7 @@ def generate_launch_description():
     state_launch_path = os.path.join(pkg_share, 'launch', 'titan_state_publisher.launch.py')
     esp_launch_path = os.path.join(esp_share, 'launch', 'esp_serial.launch.py')
     lidar_launch_path = os.path.join(lidar_share, 'launch', 'sllidar_c1_launch.py')
-    laser_filter_launch_path = os.path.join(laser_filter_share, 'examples', 'angular_filter_example.launch.py')
+    laser_filter_launch_path = os.path.join(laser_filter_share, 'examples', 'box_filter_example.launch.py')
 
     # URDF/Xacro path
     urdf_file = os.path.join(desc_share, 'urdf', 'turtlebot3_burger.urdf')
