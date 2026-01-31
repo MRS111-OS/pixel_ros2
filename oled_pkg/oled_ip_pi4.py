254925f740a8c4be5a3aa2850d9fcdcffe175989
gagasaga <gangasagar4012@gmail.com>
2026-01-31 11:32:21 +0530

    minor changes

--- oled_pkg/oled_ip_pi4.py
+++ oled_pkg/oled_ip_pi4.py
@@ -55,8 +55,9 @@ while True:
     try:
         cmd = "top -bn1 | grep '%Cpu(s)' | awk '{print 100 - $8}'"
         cpu_percent = subprocess.check_output(cmd, shell=True).decode("utf-8").strip()
-        cmd = "top -bn1 | grep load | awk '{printf \"CPU LOAD: %.2f (%s%%)\", $(NF-2), \"" + cpu_percent + "\"}'"
-        CPU = subprocess.check_output(cmd, shell=True).decode("utf-8").strip().upper()
+        cmd = "top -bn1 | grep load | awk '{printf \"CPU LOAD: %.2f\", $(NF-2)}'"
+        cpu_load = subprocess.check_output(cmd, shell=True).decode("utf-8").strip()
+        CPU = cpu_load + " (" + cpu_percent + "%)"
     except:
         CPU = "CPU LOAD: N/A"
 
